fd720586947f17c265887a68e0f972dc4e4f52ed
Sanika Mogal <sanikamogal925@gmail.com>
2025-12-25 13:08:12 +0530

    Added Files

---
+++ client/src/App.jsx
@@ -0,0 +1,54 @@
+import "./index.css";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState("");
+
+  // Register listener ONCE
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.off("message");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (messageInput.trim() !== "") {
+      socket.emit("message", messageInput);
+      setMessageInput("");
+    }
+  };
+
+ return (
+  <div className="app">
+    <h1>Simple Chat App</h1>
+
+    <div className="messages">
+      {messages.map((message, index) => (
+        <div className="message" key={index}>
+          {message}
+        </div>
+      ))}
+    </div>
+
+    <div className="input-area">
+      <input
+        type="text"
+        value={messageInput}
+        placeholder="Type your message..."
+        onChange={(e) => setMessageInput(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  </div>
+);
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,39 @@
+// Import packages
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+// Configurations
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+// Middleware
+app.use(cors());
+
+// Socket.IO logic
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("message", (message) => {
+    console.log("Message received:", message);
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Run the server
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
